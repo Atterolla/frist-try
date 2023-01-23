a60666316afabd66fbefdef4a18fa9e6c9e63581
Aliye <221288@astanait.edu.kz>
2023-01-23 11:05:13 +0600

    Adding method example to the subclass Student. This class behaves as an example

--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -25,6 +25,9 @@ public class Student extends Person {
         } else
             return 0;
     }
+    public void example(){
+        System.out.println("This is example");
+    }
     @Override
     public int compareTo(Person other) {
         return super.compareTo(other);
